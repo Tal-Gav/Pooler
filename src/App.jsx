3fc7d9090cc1a08afd065c17278de8d2fd51e8c4
Tal Gav <talgav23@gmail.com>
2023-12-19 23:16:23 +0200

    master

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Pages/Home";
 import Players from "./Pages/Players";
 import RandomBall from "./Pages/RandomBall";
@@ -7,14 +7,14 @@ import "./App.css";
 
 function App() {
   return (
-    <Router basename={`/${process.env.PUBLIC_URL}`}>
+    <BrowserRouter basename={`/pooler`}>
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/home" element={<Home />} />
+        {/* <Route path="/home" element={<Home />} /> */}
         <Route path="/players" element={<Players />} />
         <Route path="/randomball/:numOfPlayers" element={<RandomBall />} />
       </Routes>
-    </Router>
+    </BrowserRouter>
   );
 }
 
